1f0f19526c4088a43ff69040d805fe6773ca25fd
Leonard Way <87140438+Leoknowstheway@users.noreply.github.com>
2025-05-16 13:17:57 -0700

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create objects
+        // ClassName obj = new ClassName();
+
+        // Call methods
+        // obj.someMethod();
+
+        // Print output
+        System.out.println("Program started.");
+
+        // You can add more logic here to test your classes
+    }
+}
