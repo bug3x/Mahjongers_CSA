d8b8442f48bd9c0cbfc84eb5ed440da758e46b57
1961769 <1961769@01-18124.pusd.dom>
2025-05-19 12:57:00 -0700

    added gamelogictest framework

---
+++ test_scripts/GameLogicTest.java
@@ -0,0 +1,14 @@
+package test_scripts;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameLogicTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
