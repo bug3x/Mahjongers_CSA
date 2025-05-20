bfdd8f447381030779cafdb30d07d88d64d6c9cc
1961769 <1961769@01-18124.pusd.dom>
2025-05-20 14:07:05 -0700

    working on board GUI after setting up foundation of meld and tile class

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -6,7 +6,8 @@ import java.util.Stack;
 
 public class GameLogic {
 	// private List<Player> players;
-	private Board board;
+//	private Board board;
+	private Tile[][] board;
 	private Stack<Piece> wall; // the walls to take pieces from
 	
 	private int currentPlayerIndex;
@@ -14,6 +15,38 @@ public class GameLogic {
 	private boolean win;
 //	private boolean pongable, chowable, kongable;
 //	private boolean riichi;
+	
+	public GameLogic() {
+		board = new Tile[8][8];
+		
+		for(int i =0; i < board.length;i++) {
+			for(int j = 0; j < board[0].length;j++) {
+				board[i][j] = new Tile(i, j);
+			}
+		}	
+		
+		setupWalls();
+		
+		//setup other pieces
+		setupPieces(null);
+		setupPieces(null);
+	}
+	
+	public void setupPieces(Piece type) {		
+		//randomize
+		
+
+		//hands
+		
+	
+	}
+	
+	public void setupWalls() {
+		//walls
+		//for loop for each 
+		board[board.length-1][3].setPiece(new Bamboo(".png", number, board.length-2, board ));
+		board[board.length-1][4].setPiece(new Numbers("king.png", color,board.length-2,3, board ));
+	}
 	
 	public void noYaku() {
 		System.out.println("NO Yaku.");
@@ -23,6 +56,8 @@ public class GameLogic {
 	public int calcPoints() {
 		return -1;
 	}
+	
+	public 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
