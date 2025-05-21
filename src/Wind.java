4aff3d16eba5c524ea1fd5d3479c8b6fa9091cc3
natha <natha@NComputerF.san.rr.com>
2025-05-20 20:30:00 -0700

    Sick

---
+++ src/Wind.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Wind extends Piece {
+    private String direction; // "East", "South" etc.
+    private boolean isMajorWind;
+
+    public Wind(String value, String direction, boolean isMajorWind, int row, int col, String imageFileName) {
+        super("Winds", value, row, col, imageFileName);
+        this.direction = direction;
+        this.isMajorWind = isMajorWind;
+    }
+
+    @Override
+    public void operation() {
+        System.out.println("Wind direction: " + direction);
+    }
+
+    @Override
+    public void operation2() {
+        System.out.println("Is major wind: " + isMajorWind);
+    }
+}
